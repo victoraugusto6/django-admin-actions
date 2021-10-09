9f868e726fbfe83f6baf227e772dbb82f424a432
Victor Augusto <victor.augusto.soliveira@gmail.com>
2021-10-08 23:25:50 -0300

    Created model Pessoa and admin view

--- actions/action/models.py
+++ actions/action/models.py
@@ -1,3 +1,17 @@
-# from django.db import models
-#
-# # Create your models here.
+from django.db import models
+
+CATEGORIA = [
+    ('motorista', 'Motorista'),
+    ('passageiro', 'Passageiro')
+]
+
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100)
+    ativo = models.BooleanField()
+    categoria = models.CharField(choices=CATEGORIA, max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    uploaded_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nome
