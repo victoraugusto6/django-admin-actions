393e9359b5091f8a1324606d0da8d8f3acb1a176
Victor Augusto <victor.augusto.soliveira@gmail.com>
2021-10-08 23:25:50 -0300

    Created model Pessoa and admin view

--- actions/action/admin.py
+++ actions/action/admin.py
@@ -1,3 +1,9 @@
-# from django.contrib import admin
-#
-# # Register your models here.
+from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
+from actions.action.models import Pessoa
+
+
+@admin.register(Pessoa)
+class PessoaAdmin(ModelAdmin):
+    list_display = ('nome', 'ativo', 'categoria', 'created_at', 'uploaded_at')
