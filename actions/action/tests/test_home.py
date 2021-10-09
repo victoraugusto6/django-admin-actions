5c800af8d4bee240723627856b2a78e20485ef51
Victor Augusto <victor.augusto.soliveira@gmail.com>
2021-10-09 01:13:06 -0300

    Created test

---
+++ actions/action/tests/test_home.py
@@ -0,0 +1,22 @@
+import pytest
+from django.urls import reverse
+
+from django_assertions import assert_contains
+
+
+@pytest.fixture
+def resp(client):
+    resp = client.get(reverse('action:home'))
+    return resp
+
+
+def test_home_status_code(resp):
+    assert resp.status_code == 200
+
+
+def test_link_to_admin(resp):
+    assert_contains(resp, reverse('admin:index'))
+
+
+def test_button_to_admin(resp):
+    assert_contains(resp, '>Django Admin</a>')
