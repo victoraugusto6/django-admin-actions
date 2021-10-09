15aa46ede53eb1680dc94e6a281dbe285b0c230f
Victor Augusto <victor.augusto.soliveira@gmail.com>
2021-10-08 23:37:12 -0300

    Config django actions

--- actions/action/admin.py
+++ actions/action/admin.py
@@ -4,6 +4,27 @@ from django.contrib.admin import ModelAdmin
 from actions.action.models import Pessoa
 
 
+@admin.action(description='Habilitar Pessoa')
+def enable_person(modeladmin, request, queryset):
+    queryset.update(ativo=True)
+
+
+@admin.action(description='Desabilitar Pessoa')
+def disable_person(modeladmin, request, queryset):
+    queryset.update(ativo=False)
+
+
+@admin.action(description='Trocar para Motorista')
+def change_to_driver(modeladmin, request, queryset):
+    queryset.update(categoria='motorista')
+
+
+@admin.action(description='Trocar para Passageiro')
+def change_to_passenger(modeladmin, request, queryset):
+    queryset.update(categoria='passageiro')
+
+
 @admin.register(Pessoa)
 class PessoaAdmin(ModelAdmin):
     list_display = ('nome', 'ativo', 'categoria', 'created_at', 'uploaded_at')
+    actions = [enable_person, disable_person, change_to_driver, change_to_passenger]
