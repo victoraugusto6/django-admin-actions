5db08540f3ccac014d2421f2c9b8d54cfdf01dc7
Victor Augusto <victor.augusto.soliveira@gmail.com>
2021-10-08 23:15:42 -0300

    Applied django-toolbar

--- actions/settings.py
+++ actions/settings.py
@@ -53,6 +53,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# Configuração Django Debug Toolbar
+
+INTERNAL_IPS = config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
+
+if DEBUG:
+    INSTALLED_APPS.append('debug_toolbar')
+    MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+
 ROOT_URLCONF = 'actions.urls'
 
 TEMPLATES = [
